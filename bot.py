08951ef8472990a8e8a75a8464c9c3aa51a58bef
hanh2k9-dzai <taovanluc45@gmail.com>
2026-08-04 22:08:21 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ def run_health_check_server():
     server.serve_forever()
 
 # --- MAIN BOT TELEGRAM ---
-TOKEN = os.getenv("BOT_TOKEN", "8910844792:AAE2uWbQjlNgJRG1L9W7bn4yUTbeVCzQIeU")
+TOKEN = os.getenv("BOT_TOKEN", "8910844792:AAFhEQtra4DpfiRHTnkteKLnSXKR1lfVHu8")
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
